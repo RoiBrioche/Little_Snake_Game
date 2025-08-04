4221f581322507064491484d3ea23eb60e05a10b
brieuc.mandin <brieuc.mandin@etu.estia.fr>
2025-08-04 23:47:07 +0200

    création d'un score board (#9)

--- game/config.py
+++ game/config.py
@@ -5,10 +5,15 @@ WIDTH = 600
 HEIGHT = 600
 CELL_SIZE = 20
 
+# Header
+HEADER_HEIGHT = 40
+WIDTH, HEIGHT = 600, 600
+
 # Couleurs (R, G, B)
 BACKGROUND_COLOR = (30, 30, 30)
 SNAKE_COLOR = (0, 255, 0)
 FOOD_COLOR = (255, 0, 0)  # rouge
+
 
 # FPS
 TICK_RATE = 10
--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 import pygame
 import sys
 
-from game.config import WIDTH, HEIGHT, CELL_SIZE, BACKGROUND_COLOR, SNAKE_COLOR, FOOD_COLOR, TICK_RATE
+from game.config import WIDTH, HEIGHT, HEADER_HEIGHT, CELL_SIZE, BACKGROUND_COLOR, SNAKE_COLOR, FOOD_COLOR, TICK_RATE
 from game.food import Food
 from game.snake import Snake
 
 
+GRID_HEIGHT = (HEIGHT - HEADER_HEIGHT) // CELL_SIZE
 GRID_WIDTH = WIDTH // CELL_SIZE
-GRID_HEIGHT = HEIGHT // CELL_SIZE
 
 # Initialisation
 pygame.init()
@@ -20,6 +20,9 @@ pygame.display.set_caption("Snake Game")
 snake = Snake()
 
 food = Food(GRID_WIDTH, GRID_HEIGHT, snake.body)
+
+score = 0
+font = pygame.font.Font(None, 36)  # Police par défaut, taille 36
 
 # Boucle principale
 clock = pygame.time.Clock()
@@ -43,22 +46,41 @@ while running:
 
     # Logique du jeu
     snake.move()
-    if snake.check_collision(WIDTH // CELL_SIZE, HEIGHT // CELL_SIZE):
+    if snake.check_collision(GRID_WIDTH, GRID_HEIGHT):
         print("Game Over")
         running = False
+    # if snake.check_collision(WIDTH // CELL_SIZE, GRID_HEIGHT // CELL_SIZE):
+    #     game_over = True
 
     # Affichage
     SCREEN.fill(BACKGROUND_COLOR)
     for segment in snake.body:
         x, y = segment
-        pygame.draw.rect(SCREEN, SNAKE_COLOR, pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        pygame.draw.rect(
+            SCREEN, SNAKE_COLOR, pygame.Rect(x * CELL_SIZE, y * CELL_SIZE + HEADER_HEIGHT, CELL_SIZE, CELL_SIZE)
+        )
 
-    fx, fy = food.position
-    pygame.draw.rect(SCREEN, FOOD_COLOR, pygame.Rect(fx * CELL_SIZE, fy * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+    pygame.draw.rect(
+        SCREEN,
+        (255, 0, 0),
+        pygame.Rect(
+            food.position[0] * CELL_SIZE,
+            food.position[1] * CELL_SIZE + HEADER_HEIGHT,
+            CELL_SIZE,
+            CELL_SIZE,
+        ),
+    )
 
     if snake.body[0] == food.position:
         snake.grow()
         food.randomize_position(snake.body)
+        score += 1
+
+    # Bandeau
+    pygame.draw.rect(SCREEN, (50, 50, 50), pygame.Rect(0, 0, WIDTH, HEADER_HEIGHT))  # gris foncé
+    # Affichage du score
+    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
+    SCREEN.blit(score_text, (10, 10))
 
     pygame.display.flip()
     clock.tick(TICK_RATE)
