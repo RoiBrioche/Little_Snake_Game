8ec3ae222300d1a329e5f296c7a15d49d5f17d73
brieuc.mandin <brieuc.mandin@etu.estia.fr>
2025-08-12 20:14:42 +0200

    ré-implémentation du trophé

--- game/draw.py
+++ game/draw.py
@@ -1,6 +1,7 @@
 import pygame
 
 from game.config import *
+from game.graphics import draw_trophy_image
 
 GRID_HEIGHT = (HEIGHT - HEADER_HEIGHT) // CELL_SIZE
 GRID_WIDTH = WIDTH // CELL_SIZE
@@ -39,9 +40,13 @@ def draw_game_screen(screen, snake, food, score, best_score, game_over, font):
 
     # Dessiner la barre de score
     pygame.draw.rect(screen, (50, 50, 50), pygame.Rect(0, 0, WIDTH, HEADER_HEIGHT))
+    
 
     score_text = font.render(f"Score: {score}", True, (255, 255, 255))
     screen.blit(score_text, (10, 10))
+
+    # Afficher le trophée à gauche du best score
+    draw_trophy_image(screen, WIDTH - 210, 5)
 
     best_score_text = font.render(f"Best: {best_score}", True, (255, 215, 0))
     screen.blit(best_score_text, (WIDTH - 150, 10))
